b744c18c79cab47c86086fca04b0fec10efa7e31
kmdiogo <kmdiogo@student.ysu.edu>
2018-10-17 18:51:04 -0400

    BIG CHANGES, BIG TINGS - Refactored all id names to adhere to our naming standards - added submit button for y number

--- SignIn/views.py
+++ SignIn/views.py
@@ -5,12 +5,14 @@ from django.views.decorators.http import require_POST, require_http_methods, req
 from django.http import JsonResponse, HttpResponse
 from django.template.loader import render_to_string
 
+
 @require_http_methods(['GET'])
 def index(request):
     form = StudentForm()
 
     context = {'form': form}
     return render(request, 'SignIn/Index.html', context)
+
 
 @require_POST
 def create_student(request):
@@ -27,12 +29,12 @@ def create_student(request):
         data = {'student_is_valid': False}
         return JsonResponse(data)
 
+
 @require_GET
 def school_id_taken(request):
     id = request.GET.get('schoolId', None)
     response = {'isTaken': Student.objects.filter(schoolId__iexact=id).exists()}
     return JsonResponse(response)
-
 
 
 @require_GET
